b3e4de4fc4e31c5d42db27407a0d5d9ea6b81acb
unknown <jake@goldcoastmaids.com.au>
2019-01-25 20:20:02 +0700

    feat: add image compression plugin

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 const withSize = require('next-size')
+const withOptimizedImages = require('next-optimized-images');
 
-module.exports = withSize()
+module.exports = withSize(withOptimizedImages({
+    /* config for next-optimized-images */
+
+    // your config for other plugins or the general next.js here...
+}));
